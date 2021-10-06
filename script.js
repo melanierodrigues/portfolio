26aeacbf17c677fccd4eec23e94a55c2382faec4
Mélanie Rodrigues <melanierodrigues.mail@gmail.com>
2021-10-06 14:24:39 +0100

    Test2 - Fullscreen

--- script.js
+++ script.js
@@ -71,7 +71,7 @@ observer = new IntersectionObserver ((entries) => {
             }
         }
         requestAnimationFrame(hello1);
-        openFullscreen();
+        requestFullscreen();
     })
 });
 
@@ -118,7 +118,7 @@ observer = new IntersectionObserver ((entries) => {
             }
         }
         requestAnimationFrame(projecto1);
-        openFullscreen();
+        requestFullscreen();
     })
 });
 
@@ -188,7 +188,7 @@ observer = new IntersectionObserver ((entries) => {
             }
         }
         requestAnimationFrame(projecto2);
-        openFullscreen();
+        requestFullscreen();
     })
 });
 
@@ -247,7 +247,7 @@ observer = new IntersectionObserver ((entries) => {
             }
         }
         requestAnimationFrame(projecto3);
-        openFullscreen();
+        requestFullscreen();
     })
 });
 
@@ -280,7 +280,7 @@ observer = new IntersectionObserver ((entries) => {
             }
         }
         requestAnimationFrame(aboutme1);
-        openFullscreen();
+        requestFullscreen();
     })
 });
 
