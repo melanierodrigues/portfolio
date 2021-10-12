8b4c78455f8d4f87106697e50661dfb95763e6b3
Mélanie Rodrigues <melanierodrigues.mail@gmail.com>
2021-10-12 11:50:57 +0100

    Scroll

--- script.js
+++ script.js
@@ -3,11 +3,12 @@
 
 window.addEventListener("load", function() {
 
-// Altura da viewport multiplicada por 1% para obter um valor para vh
-let vh = window.innerHeight * 0.01;
- 
-// Configura o valor em --vh na raiz do documento
-document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+    window.addEventListener('resize', () => {
+      // Executa o mesmo script de antes
+      let vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    });
 
 
     var loading = document.getElementById('loading');
