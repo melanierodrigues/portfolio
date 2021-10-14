62d54f3bf085534e2ce63fab08dccda41237e84a
Mélanie Rodrigues <melanierodrigues.mail@gmail.com>
2021-10-14 10:48:24 +0100

    teste3

--- script.js
+++ script.js
@@ -73,8 +73,8 @@ function handleOrientation(event) {
   // It center the positioning point to the center of the ball
   
  
-  ball.style.top  = (maxY*y/180 - 10) + "px";
-  ball.style.left = (maxX*x/180 - 10) + "px";
+  ball.style.top  = (maxY*y/180 + 200) + "px";
+  ball.style.left = (maxX*x/180 + 0) + "px";
 }
 
 window.addEventListener('deviceorientation', handleOrientation);
